367a73d7310256976266d6a49f9b47783d85c26e
nancymoreen <nancymoreen@gmail.com>
2023-05-15 19:02:48 -0700

    is palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,53 +1,37 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
-*add_nodeint - adds a new node at the beginning of a listint_t list
-*@head: head of listint_t
-*@n: int to add in listint_t list
-*Return: address of the new element, or NULL if it failed
-*/
-listint_t *add_nodeint(listint_t **head, const int n)
+ * _is_palindrome - determine if a singly linked list is a palindrome (helper)
+ * @head: a pointer to a pointer to the start of the list
+ *
+ * Return: 1 if the list is a palindrome, otherwise 0
+ */
+int _is_palindrome(listint_t **head, listint_t *tail)
 {
-	listint_t *new;
+	int status = 1;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	new->next = *head;
-	*head = new;
-	return (new);
+	if (tail)
+	{
+		status = _is_palindrome(head, tail->next);
+
+		if (tail == *head || tail->next == *head)
+			*head = tail;
+		else if (status && (*head)->n == tail->n)
+			*head = (*head)->next;
+		else
+			status = 0;
+	}
+	return (status);
 }
+
+
 /**
-*is_palindrome - identify if a syngle linked list is palindrome
-*@head: head of listint_t
-*Return: 1 if it is palindrome else 0
-*/
+ * is_palindrome - determine if a singly linked list is a palindrome
+ * @head: a pointer to a pointer to the start of the list
+ *
+ * Return: 1 if the list is a palindrome, otherwise 0
+ */
 int is_palindrome(listint_t **head)
 {
-	listint_t *head2 = *head;
-	listint_t *aux = NULL, *aux2 = NULL;
-
-	if (*head == NULL || head2->next == NULL)
-		return (1);
-	while (head2 != NULL)
-	{
-		add_nodeint(&aux, head2->n);
-		head2 = head2->next;
-	}
-	aux2 = aux;
-	while (*head != NULL)
-	{
-		if ((*head)->n != aux2->n)
-		{
-			free_listint(aux);
-			return (0);
-		}
-		*head = (*head)->next;
-		aux2 = aux2->next;
-	}
-	free_listint(aux);
-	return (1);
+	return (head && _is_palindrome(head, *head));
 }
