3617deb568103f0724aeb3d7e5168402fa1919e8
nancymoreen <nancymoreen@gmail.com>
2023-05-29 15:01:09 -0700

    Handling exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        count = 0
-        for n in my_list[:x]:
-            try:
-                print(":{d}".format(n), end=' ')
-                count += 1
-            except ValueError:
-                pass
-            except TypeError:
-                raise
-            finally:
-                print()
-                return count
+    count = 0
+    for n in range(x):
+        try:
+            print("{:d}".format(my_list[n]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return (count)
