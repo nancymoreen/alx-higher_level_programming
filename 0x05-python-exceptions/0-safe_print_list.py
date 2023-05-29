6d3be421691c61983283818fa9bb34716a905af2
nancymoreen <nancymoreen@gmail.com>
2023-05-29 14:58:21 -0700

    Handling exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-    try:
-        count = 0
-        for n in my_list[:x]:
-            print(n, end=' ')
+    count = 0
+    for n in range(x):
+        try:
+            print("{}".format(my_list[n]), end="")
+        except IndexError:
+            break
+        else:
             count += 1
-    except TypeError as e:
-        print("Error:", e)
-    finally:
-        print()
-        return count
+    print()
+    return (count)
