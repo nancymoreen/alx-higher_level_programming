cc261afcf4eb7feada0b796bac3467a11475e444
nancymoreen <nancymoreen@gmail.com>
2023-05-30 14:17:36 -0700

    Defines a square based on given size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Creation of class Square"""
+
+class Square:
+
+    """Defines a square"""
+
+    def __init__(self, size):
+
+        """Use of given size.
+        Args:
+        size: Size of the square.
+        """
+
+        self.__size = size
