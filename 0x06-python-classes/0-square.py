9201ae76c7125193dcd55669ec79f6f6098c0816
nancymoreen <nancymoreen@gmail.com>
2023-05-30 14:53:28 -0700

    Defines a square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
 """Creation of an empty class named square"""
 
+
 class Square:
-    """Class Square defines a square"""
+    """Empty class Square defines a square"""
     pass
