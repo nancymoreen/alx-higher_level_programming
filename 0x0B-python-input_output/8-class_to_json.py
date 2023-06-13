dbb59071481ac160d6b438fda98e2bb1d1b5af8a
nancymoreen <nancymoreen@gmail.com>
2023-06-13 12:23:54 -0700

    Returns the dict description nwith simple data structure

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
 """Module that returns the dictionary description with simple data structure
-for JSON serialization of an object.
 """
 
 
 def class_to_json(obj):
-    """Function that eturns the dictionary description with simple data
-    structure for JSON serialization of an object.
+    """Method that returns the dictionary description
+    with simple data structure for SON serialization of an object
     """
-    if isinstance(obj, str) or isinstance(obj, int) or isinstance(obj, bool):
-        return obj
-    elif isinstance(obj, list):
-        return [class_to_json(item) for item in obj]
-    elif isinstance(obj, dict):
-        return {key: class_to_json(value) for key, valuein obj.items()}
+    if hasattr(obj, "__dict__"):
+        return obj.__dict__.copy()
     else:
-        return obj.__dict__
+        return {}
