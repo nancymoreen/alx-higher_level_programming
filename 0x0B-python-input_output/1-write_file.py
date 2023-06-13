da1954d52af83ca6f99b3f3d546a25580ff54597
nancymoreen <nancymoreen@gmail.com>
2023-06-13 10:38:50 -0700

    Writes a string to a text file and returns the number of charaters written

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,10 +1,12 @@
 #!/usr/bin/bash
-"""Module that writes a string to a text file and returns the number of charaters written.
+"""Module that writes a string to a text file and returns the number of
+characters written.
 """
 
 
 def write_file(filename="", text=""):
-    """Funtion that writes a string to a text file and returns the number of characters written.
+    """Function that writes a string to a text file and returns the number of
+    characters written.
     """
     with open(filename, 'w', encoding='utf-8') as file:
         file.write(text)
