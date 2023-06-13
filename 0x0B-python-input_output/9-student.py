671380e7c95437a1097933e94936014fc848545a
nancymoreen <nancymoreen@gmail.com>
2023-06-13 12:35:47 -0700

    Dictionary representation of Student instance

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module for defining a Student
+"""
+
+
+class Student:
+    def __init__(self, first_name:str, last_name:str, age:int) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+        def to_json(self) -> dict:
+            """Dictionary representation of Student instance.
+            """
+            return {
+                    'first_name' : self.first_name,
+                    'last_name' : self.last_name,
+                    'age' : self.age
+                    }
