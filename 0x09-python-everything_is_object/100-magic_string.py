39c004bfc5de5735a156017a122ba20c8d7a890f
nancymoreen <nancymoreen@gmail.com>
2023-06-23 13:22:22 -0700

    Magic string

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-def magic_string(list_print=[]):
-    list_print += ["Holberton"]
-    return (", ".join(list_print))
+def magic_string():
+    return ', '.join(['BestSchool'] * (i + 1) for i in range(10))
