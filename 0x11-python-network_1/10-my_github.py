d12aabc486b9bf4e2a295e4688ea21c4f2815708
nancymoreen <nancymoreen@gmail.com>
2023-09-04 08:46:46 -0700

    Task 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,26 +2,9 @@
 """takes Github credentials to display id"""
 import sys
 import requests
+from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-
-    url = "https://api.github.com/user"
-
-    token = f"{username}:{password}"
-    encoded_token = token.encode("utf-8").b64encode().decode("utf-8")
-
-    headers = {
-        "Authorization": f"Basic {encoded_token}",
-    }
-
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        user_data = response.json()
-        user_id = user_data.get("id")
-        print(user_id)
-    else:
-        print(None)
-
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
