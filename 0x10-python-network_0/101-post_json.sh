fcaff156ff8d2846a3dfb2b82348f582a6b5fc51
nancymoreen <nancymoreen@gmail.com>
2023-09-04 19:33:40 -0700

    Task 101

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Bash script that sends a JSON POST request to a URL
-# passed as the first argument, and displays the body of the response.
+# Bash script that sends a JSON POST request to a URL passed as the first argument
 curl -s -H "Content-Type: application/json" -X POST -d @"$2" "$1"
