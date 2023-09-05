6f458886b52529ee9687713d27a6ce19c441e74c
nancymoreen <nancymoreen@gmail.com>
2023-09-05 14:48:17 -0700

    Task 1

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""List all states"""
+"""List all states
+from the database"""
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import (create_engine)
